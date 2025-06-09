e5d3afba7df75def03fec528b5d8c5b20e1df5a9
Gaddiel05 <gaddielirakoze@gmail.com>
2025-06-09 12:26:12 +0200

    summative commit

--- load_data.py
+++ load_data.py
@@ -1,7 +1,7 @@
 from parse_sms import parse_sms_xml
 from insert_data import insert_messages
 
-file_path = "modified_sms_v2.xml"
+file_path = "./Data/modified_sms_v2.xml"
 
 # Step 1: Parse the XML
 messages = parse_sms_xml(file_path)
